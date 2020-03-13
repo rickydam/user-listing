fa206038431ea0380a71283c0b471d3ba1742e64
Ricky Dam <RickyDamOttawa@gmail.com>
2020-03-12 21:12:00 -0400

    Generate a migration to add columns to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_014151) do
+ActiveRecord::Schema.define(version: 2020_03_13_010933) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cover_url"
+    t.integer "average_rating"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
