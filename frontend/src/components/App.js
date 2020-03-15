dc57c01ffd8d3539388b8e767b86b0e8ab6cf038
Ricky Dam <RickyDamOttawa@gmail.com>
2020-03-14 20:12:30 -0400

    Render Users component in root component App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Users from './Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container mx-auto px-4">
+        <Users />
+      </div>
+    );
+  }
 }
 
 export default App;
