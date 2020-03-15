5e4447d9f043de23f604aa7519a59b782d3f8f27
Ricky Dam <RickyDamOttawa@gmail.com>
2020-03-15 17:25:46 -0400

    Create Books component to load user books

---
+++ frontend/src/components/Books.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+const Books = ({ books }) => (
+  <Fragment>
+    {books.map((book) =>
+      <div className="flex border-b border-solid border-grey-light">
+        <div className="w-3/4 p-4">
+          <h3>{book.title}</h3>
+          <p className="text-grey-darker">
+            {[...Array(book.averageRating).keys()].map((s) =>
+              <span key={s}>&#9733;</span>
+            )}
+          </p>
+        </div>
+        <div className="w-1/4 p-4 text-right">
+          <img src={book.coverUrl} alt={book.title} />
+        </div>
+      </div>
+    )}
+  </Fragment>
+);
+
+export default Books;
